ae17946967645a93d1cbaca79efade4862c16a13
hdp0545 <hdp0545@gmail.com>
2020-12-10 15:59:12 +0900

    Edit Debug settings to False

--- backend/myapi/settings.py
+++ backend/myapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%skm1!4(&bep08%1kwzi$wr8t0b#8%5t!0yy4gjxs4l+6(w5$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
