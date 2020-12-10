9788a8e72c966443ef1a478f2523784625967567
hdp0545 <hdp0545@gmail.com>
2020-12-10 22:37:03 +0900

    Add img Url

--- backend/img/views.py
+++ backend/img/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from .serializers import ImagesSerializer
+
+# models
 from .models import Images
-from rest_framework import viewsets
+from .serializers import ImagesSerializer
 # Create your views here.
+
+# forms
+from .forms import UploadFileForm
+
+# rest_framework
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 
 class ImagesViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
+
+@api_view(['POST'])
+def save_img(request):
+    if request.method == 'POST':
+        form = 
+        if form.is_valid()
+        
