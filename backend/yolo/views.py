cbfa35c63885d997ebf00e92ce3a4aae474693e4
hdp0545 <hdp0545@gmail.com>
2020-12-17 23:42:10 +0900

    Add Video Streaming

--- backend/yolo/views.py
+++ backend/yolo/views.py
@@ -2,26 +2,22 @@ from django.shortcuts import render
 import cv2
 import threading
 from django.http import StreamingHttpResponse
-from django.views.decorators import gzip
 
 
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
+        # threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        image = self.frame
+        grabbed, frame = self.video.read()
+        image = frame
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
-    def update(self):
-        while True:
-            (self.grabbed, self.frame) = self.video.read()
 
 
 cam = VideoCamera()
@@ -34,9 +30,7 @@ def gen(camera):
               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@gzip.gzip_page
 def livefe(request):
-    try:
-        return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        pass
+    return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
+  
+
