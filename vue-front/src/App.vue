f811183df2bc0f83ad40935e39aadb9b6b9184ef
hdp0545 <hdp0545@gmail.com>
2020-12-20 22:11:47 +0900

    Add Vueproject

---
+++ vue-front/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <a href="/">
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+      </a>
+
+      <v-spacer></v-spacer>
+      <v-tabs
+        dark
+        optional
+        right
+        >
+        <v-tab
+          to="/about"
+          text
+        >
+          <span class="mr-2">About Us</span>
+        </v-tab>
+        <v-tab
+          to="/convert"
+          text
+        >
+          <span class="mr-2">Convert</span>
+        </v-tab>
+        <v-tab
+          to="/contact"
+          text
+        >
+          <span class="mr-2">Contact Us</span>
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <footer-vue/>
+  </v-app>
+</template>
+
+<script>
+import footerVue from '@/components/Footer.vue'
+export default {
+  name: 'App',
+
+  components: {
+    footerVue,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
