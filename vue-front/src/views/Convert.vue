5e4bc27e3cb2e718423e0d4a56ec4983fbc0dbfc
hdp0545 <hdp0545@gmail.com>
2020-12-21 01:52:26 +0900

    Add File-Upload

--- vue-front/src/views/Convert.vue
+++ vue-front/src/views/Convert.vue
@@ -1,39 +1,15 @@
 <template>
   <div>
-    <div v-if="currentFile" class="progress">
-      <div
-        class="progress-bar progress-bar-info progress-bar-striped"
-        role="progressbar"
-        :aria-valuenow="progress"
-        aria-valuemin="0"
-        aria-valuemax="100"
-        :style="{ width: progress + '%' }"
-      >
-        {{ progress }}%
-      </div>
-    </div>
-    <img class="img-video" src='../img/icon-video.png' alt="비디오 로고" height="100"/>
-    <label class="selectbtn btn btn-default">
-      <!-- <input type="file"  /> -->
-      <input type="text" value="선택된 파일이 없습니다." id="fileName" class="file_input_textbox" readonly="readonly">
-      <input type="button" value="파일 선택" class="file_input_button font-weight-bold"/>
-      <input type="file" ref="file" @change="selectFile" class="file_input_hidden" onchange="javascript:document.getElementById('fileName').value = this.value.split('\\')[this.value.split('\\').length-1]">
-    </label>
-    <!-- :disabled="!selectedFiles" -->
+    <upload-box></upload-box>
   </div>
 </template>
 
 <script>
+import UploadBox from '../components/UploadBox.vue'
 export default {
   name: "Convert",
-  data() {
-    return {
-      selectedFiles: undefined,
-      currentFile: undefined,
-      progress: 0,
-      message: "",
-      fileInfos: [],
-    }
+  components: {
+    UploadBox
   }
 }
 </script>
